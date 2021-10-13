17c9a76c2a85c5fe099c60d739c02885cfd2f026
Bartosz Galek <bartosz.galek@allegro.pl>
2021-10-13 13:16:08 +0200

    method extracted

--- action.js
+++ action.js
@@ -11,18 +11,7 @@ module.exports = async function publishPrototypes(host, scId, files, path, apiTo
   for (const prototypeFile of changedPrototypes) {
     core.info(`updating ${prototypeFile}`);
     const prototype = String(fs.readFileSync(prototypeFile, 'utf8'));
-    const response = await fetch(
-      `https://${host}/api/management/prototypes`,
-      {
-        method: 'POST',
-        headers: {
-          authorization: `Bearer ${apiToken}`,
-          accept: 'application/vnd.opbox-page-manager.v2+json',
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify({ serviceCatalogId: scId, repositoryUrl, prototype: JSON.parse(prototype) })
-      }
-    );
+    const response = await postPrototype(prototype);
     console.log(response.status, await response.json());
   }
 
@@ -37,13 +26,32 @@ module.exports = async function publishPrototypes(host, scId, files, path, apiTo
     const path = file.replace('.deprecated', '');
     const anyFileFound = fs.readdirSync(path).filter(it => it.endsWith('.json'))[0];
     const prototype = JSON.parse(String(fs.readFileSync(path + anyFileFound, 'utf8')));
-    const response = await fetch(`https://${host}/api/management/prototypes/${prototype.kind}/${prototype.id}/deprecated`, {
+    const response = await deprecatePrototype(prototype, deprecated);
+    console.log(response.status, await response.json());
+  }
+
+  function deprecatePrototype(prototype, deprecated) {
+    return fetch(`https://${host}/api/management/prototypes/${prototype.kind}/${prototype.id}/deprecated`, {
       method: deprecated ? 'POST' : 'DELETE',
       headers: {
         accept: 'application/vnd.opbox-page-manager.v2+json',
         authorization: `Bearer ${apiToken}`
       }
     });
-    console.log(response.status, await response.json());
+  }
+
+  function postPrototype(prototype) {
+    return fetch(
+      `https://${host}/api/management/prototypes`,
+      {
+        method: 'POST',
+        headers: {
+          authorization: `Bearer ${apiToken}`,
+          accept: 'application/vnd.opbox-page-manager.v2+json',
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({ serviceCatalogId: scId, repositoryUrl, prototype: JSON.parse(prototype) })
+      }
+    );
   }
 };
